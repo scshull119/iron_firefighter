8720e0e88d94306a77cd4311a15e433da0c5f103
Sam Shull <sshull@sailthru.com>
2022-08-18 14:01:08 -0500

    [OVER_AND_OUT] better vegas coordinates

--- index.js
+++ index.js
@@ -72,7 +72,7 @@ async function run() {
     }
 
     console.log('Iron Man puts out all the fires. Yay!')
-    await animator.travel({ x: 1150, y: 900 });
+    await animator.travel({ x: 1160, y: 920 });
     await animator.overAndOut();
 }
 
