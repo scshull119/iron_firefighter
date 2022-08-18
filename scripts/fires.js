d6c8b357ae907f0fd839d369f2b800e93031fed0
Sam Shull <sshull@sailthru.com>
2022-08-18 13:39:46 -0500

    [SHRINKING_FIRES] shrinking fires working

--- scripts/fires.js
+++ scripts/fires.js
@@ -16,14 +16,33 @@ function initFires(){
         fireElements[idx].style.top = `${fire.y}px`;
         fireElements[idx].style.left = `${fire.x}px`;
         fireElements[idx].style.display = 'block';
+        fireElements[idx].style.height = '100px';
     });
 }
 
 function putOutFire({ x: xLoc, y: yLoc}) {
-    fires.forEach((fire, idx) => {
-        if (fire.x === xLoc && fire.y === yLoc) {
-            fireElements[idx].style.display = 'none';
+    return new Promise((resolve, reject) => {
+        let fireEl;
+        fires.forEach((fire, idx) => {
+            if (fire.x === xLoc && fire.y === yLoc) {
+                fireEl = fireElements[idx];
+            }
+        });
+        if (!fireEl) {
+            reject('No fire found at this location');
         }
+
+        const shrinker = setInterval(() => {
+            let elSize = parseInt(fireEl.style.height);
+            elSize -= 10;
+            if (elSize <= 0) {
+                fireEl.style.display = 'none';
+                clearInterval(shrinker);
+                resolve('Fire is out');
+            } else {
+                fireEl.style.height = `${elSize}px`;
+            }
+        }, 100);
     });
 }
 
